1921593113874b4c6725a9ae80248739f54592ac
Hyuga Tsukui <gakikiki407@gmail.com>
2023-07-22 19:36:05 +0900

    Hello, CDK!

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,19 +1,18 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 
-export class CdkWorkshopStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class CdkWorkshopStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const hello = new cdk.aws_lambda_nodejs.NodejsFunction(
+      this,
+      "HelloHandler",
+      {
+        runtime: cdk.aws_lambda.Runtime.NODEJS_18_X,
+        entry: "lambda/hello.ts",
+        handler: "handler",
+      }
+    );
   }
 }
